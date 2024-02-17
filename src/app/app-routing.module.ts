b30752717770dcc5a9f695bf917e819d1ff18b5f
elliya <80318068+abeelly@users.noreply.github.com>
2024-02-17 23:12:28 +0800

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { SearchResultComponent } from './searchresult/searchresult.component';
+import { SearchPageComponent } from './searchpage/searchpage.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'searchpage', component: SearchPageComponent},
+  { path: 'search', component: SearchResultComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
---
+++ src/app/searchpage/searchpage.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { YoutubeService } from '../youtubeservice.service';
+
+@Component({
+  selector: 'app-searchpage',
+  templateUrl: './searchpage.component.html',
+  styleUrls: ['./searchpage.component.css']
+})
+export class SearchPageComponent {
+  searchQuery: string = '';
+
+  constructor(private youtubeService: YoutubeService, private router: Router) {}
+
+  searchVideos() {
+    if (this.searchQuery.trim() !== '') {
+      this.router.navigate(['/search'], { queryParams: { q: this.searchQuery } });
+    }
+  }
+}
+
